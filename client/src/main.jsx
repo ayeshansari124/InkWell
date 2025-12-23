0b69608a2a02b474184c2d560b4d9897b0694cc3
Ayeshaali124 <ayeshansari124@gmail.com>
2025-12-23 18:12:11 +0530

    fixed mime error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app/App";
+import App from "@/app/App";
 import { UserContextProvider } from "./context/UserContext";
 import "./styles/index.css";
 
